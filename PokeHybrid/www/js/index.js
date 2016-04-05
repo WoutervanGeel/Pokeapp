497e5f3ec2b843318a973a372af4619773676dec
WoutervanGeel <w.vangeel@student.avans.nl>
2016-04-05 20:45:07 +0200

    added a lot

--- PokeHybrid/www/js/index.js
+++ PokeHybrid/www/js/index.js
@@ -5,120 +5,128 @@ function Application(){
 	var self = this;
 	var api = new APIhandler();
     var selectedPokemon;
-    var triggerCell;
+    var triggerCell = 'load';
     var number = 1;
-    	
-    // START OVER ---------------------------------------------------------------------------------------------------------------
+    var allPokemon = [];
+    var onPokemonListPage = true;
+   
+    self.onDeviceReady = function() {
+        self.addBatteryListeners();
+        self.addClickEventsToList();
+        self.addCameraButtonEvent();
+        self.addPageEvents();
+        self.requestNewPokemon();
+    }
     
-    
-    // $(document).on('pagecreate', '#index-page', function(){
-    //     api.getAllPokemon(function(data){
-    //         for(xPokemon in data){
-    //             var pokemon = data[xPokemon];
-    //             $('#pokemon-list').append("<li id='"+pokemon['name']+"'><a href='#detail-page'>"+pokemon['name']+"</a></li>");
-    //             //$('#pokemon-list').append("<li id='pId"+pokemon['name']+"'><a href='#detail-page'>"+pokemon['name']+"</a></li>");
-                
-                
-    //             $('#'+pokemon['name']).on('click', function(e){
-    //                e.preventDefault();
-    //                self.selectedPokemon = this.id;
-    //                $.mobile.changePage('#detail-page');
-    //             });
-    //         }
-    //         $('#pokemon-list').listview('refresh');
-    //     });
-    // });
-    
-    $(document).on('pagecreate', '#index-page', function(){
-       $(document).on("scrollstop", function(){
+    self.addPageEvents = function(){
+        $("#detail-page").on("swiperight", function(){
+            //implement swipe right switch on single pokemon
+            //TODO implement switch
+            onPokemonListPage = true;
+            $.mobile.changePage('#index-page');
+        });
+       
+        $(document).on('pagebeforeshow', '#detail-page', function(){
+            onPokemonListPage = false;
+            if(self.selectedPokemon.types == null || self.selectedPokemon.moves == null){
+                self.requestSinglePokemon();
+            }
+            self.drawDetailView();
+        });
+       
+        $("#camera-page").on("swipeleft", function(){
+            //implement swipe right switch on single pokemon
+            //TODO implement switch
+            //alert("swiping right");
+            onPokemonListPage = true;
+            $.mobile.changePage('#index-page');
+        });
+       
+        $(document).on('pagebeforeshow', '#index-page', function(){
+            //alert("before indexPage show");
+            onPokemonListPage = true;
+        });
+       
+        $(document).on("scrollstop", function(){
             //Check if TriggerCell is on screen
-            var element = $('#'+self.triggerCell);
-            if(self.isOnScreen(element)){
-                self.addPokemonToList();
+            if(onPokemonListPage){
+                var element = $('#'+triggerCell);
+                if(self.isOnScreen(element)){
+                    //alert("requesting new pokemon");
+                    self.requestNewPokemon();
+                }
             }
         });
         
-        self.addPokemonToList();
-    });
-    
-    self.addPokemonToList = function(){
-        api.getMorePokemon(function(data){
-            var half = data.length - 10;
-            var i = 0;
-            for(xPokemon in data){
-                var pokemon = data[xPokemon];
-                $('#pokemon-list').append("<li id='"+pokemon['name']+"'><a href='#detail-page'>"+number+" - "+pokemon['name']+"</a></li>");
-                number++;
-                //$('#pokemon-list').append("<li id='pId"+pokemon['name']+"'><a href='#detail-page'>"+pokemon['name']+"</a></li>");
-                
-                
-                $('#'+pokemon['name']).on('click', function(e){
-                   e.preventDefault();
-                   self.selectedPokemon = this.id;
-                   $.mobile.changePage('#detail-page');
-                });
-                
-                if(i == half){
-                    //apply to the cell in the middle
-                    self.triggerCell = pokemon['name'];
-                }
-                
-                i++;
-            }
-            $('#pokemon-list').listview('refresh');
+        $("#index-page").on("swiperight", function(){
+            $.mobile.changePage('#camera-page');
+            onPokemonListPage = false;
+            
         });
     }
     
-    $(document).on('pagebeforeshow', '#detail-page', function(){
-        $('#pokemon-name').html(self.selectedPokemon);
-        $('#types').empty();
-        $('#moves').empty();
-        
-        api.getPokemonByName(self.selectedPokemon, function(data){
+    self.addCameraButtonEvent = function(){
+        $("#camera-button").on('click', function(e){
             
-            //add types
-            $('#types').append("<li data-role='list-divider' data-theme='b'>Types</li>");
-            for(xType in data['types']){
-                $('#types').append("<li>"+ data['types'][xType] +"</li>")
-            }
-            $('#types').listview('refresh');
-            
-            //add moves
-            $('#moves').append("<li data-role='list-divider' data-theme='b'>Moves</li>");
-            for(xMove in data['moves']){
-                $('#moves').append("<li>"+ data['moves'][xMove] +"</li>")
-            }
-            $('#moves').listview('refresh');
+            navigator.camera.getPicture( self.cameraSuccess, self.cameraError, {
+                quality: 50, // Foto kwaliteit
+                destinationType: Camera.DestinationType.DATA_URL // Base64 gecodeerde afbeelding als resultaat
+            });
         });
-    });
+    }
     
-    // function elementInViewport(el) {
-    //     var top = el.offsetTop;
-    //     var left = el.offsetLeft;
-    //     var width = el.offsetWidth;
-    //     var height = el.offsetHeight;
+    self.battCrit = function(info){
+        alert("batcrit called!");
+        navigator.notification.alert("Your battery is SUPER low!");
+        self.drawStatus(info);
+    }
+    
+    self.battLow = function(info){
+        alert("batlow called!");
+        navigator.notification.alert("Your battery is low!");
+        self.drawStatus(info);
+    }
+    
+    self.cameraSuccess = function(imageData){
+        $('#image').html('<img style="width: 100%;" src="' + "data:image/jpeg;base64," + imageData + '">');
+    }
         
-    //     var bottom = top + height;
-    //     var bottomScreen = window.pageYOffset;
-    //     console.log(el);
-    //     alert("element: "+el+"\n\nheight element: "+height+"\ntop element: "+top+"\nbottom element: "+bottom+"\nbottomScreen: "+bottomScreen);
+    self.cameraError = function(message){
+        //alert("Something went wrong" + message);
+    }
+    
+    self.battStat = function(info){
+        self.drawStatus(info);
+    }
+    
+    self.drawStatus = function(info){
+        $('#battery-status').html("Battery: "+info.level +"%");
+    };
+    
+    self.addBatteryListeners = function(){
+        window.addEventListener("batterycritical", self.battCrit, false);
+        window.addEventListener("batterylow", self.battLow, false);
+        window.addEventListener("batterystatus", self.battStat, false);
+    }
+    
+    self.drawPokemonList = function(){
+        //This will draw all pokemon
+        number = 1;
+        $('#pokemon-list').empty();
+        
+        //alert(allPokemon);
+        if(allPokemon != undefined){
+            for(xPokemon in allPokemon){
+                $('#pokemon-list').append("<li id='"+allPokemon[xPokemon].name+"'><a href='#detail-page'>"+number + " - " + allPokemon[xPokemon].name+"</a></li>");
+                number++;
+            }
+            $('#pokemon-list').append("<li id='load'>Load more</li>");
+        }
+        
+        $('#pokemon-list').listview('refresh');
+    }
+    
 
-    //     // while(el.offsetParent) {
-    //     //     el = el.offsetParent;
-    //     //     top += el.offsetTop;
-    //     //     left += el.offsetLeft;
-    //     // }
-
-
-    //     var arg1 = (top >= window.pageYOffset);
-    //     alert(arg1);
-    //     return (
-    //         top >= window.pageYOffset /*&&
-    //         left >= window.pageXOffset &&
-    //         (top + height) <= (window.pageYOffset + window.innerHeight) &&
-    //         (left + width) <= (window.pageXOffset + window.innerWidth)*/
-    //     );
-    // }
     
     self.isOnScreen = function(element){
         var viewport = {};
@@ -130,199 +138,122 @@ function Application(){
         return ((bounds.top <= viewport.bottom) && (bounds.bottom >= viewport.top));
     };
     
-}
-    
-    // START OVER ---------------------------------------------------------------------------------------------------------------
-    
-    // self.bindEvents = function() {
-    //     document.addEventListener('deviceready', this.onDeviceReady, false);
-    // }
-    
-    // self.onDeviceReady = function() {
-    //     app.receivedEvent('deviceready');
-    // }
-    
-    // self.receivedEvent = function(id) {
-    //     var parentElement = document.getElementById(id);
-    //     var listeningElement = parentElement.querySelector('.listening');
-    //     var receivedElement = parentElement.querySelector('.received');
-
-    //     listeningElement.setAttribute('style', 'display:none;');
-    //     receivedElement.setAttribute('style', 'display:block;');
-
-    //     console.log('Received Event: ' + id);
-    // }
-    /*
-    $(document).on('pagecreate', '#detail-page', function(){
-        $('#pokemon-name').html(self.selectedPokemon.name);
-        $('#types').empty();
-        
-        for(xType in self.selectedPokemon.types){
-            $("<li><a href='#'>"+self.selectedPokemon.types[xType]+"</a></li>").appendTo("#types").listview();
-        }
-    });
-    
-    self.refreshDetailPage = function(){
-        $('#pokemon-name').html(self.selectedPokemon.name);
-        
-        $('#types').empty();
-        for(xType in self.selectedPokemon.types){
-            $("<li><a href='#'>"+self.selectedPokemon.types[xType]+"</a></li>").appendTo("#types").listview();
-            //$('#types').append("<li><a href='#'>"+self.selectedPokemon.types[xType]+"</a></li>");
-            //$("#types").append("<li><a href='#'>"+self.selectedPokemon.name+"</a></li>");
-        }
-        var lv = $('#types');
-        console.log(lv);
-        //$('#types').listview('refresh');
+    self.bindEvents = function() {
+        document.addEventListener('deviceready', self.onDeviceReady, false);
     }
     
-    self.addPokemonToList = function(pokemon){
-        $("#pokemon-list").append("<li id='pId"+pokemon.name+"'><a href='#detail-page'>"+pokemon.name+"</a></li>");
-        
-        $('#pId' + pokemon.name).on('click', function(e) {
-            e.preventDefault();
-            //alert('test -'+pokemon.name + "-");
-            self.selectedPokemon = pokemon.name;
-            self.refreshDetailPage();
-            $.mobile.changePage('#detail-page');
-            //$("#pokemon-list").append("<li><a href='#'>test</a></li>");
+    self.requestNewPokemon = function(){
+        api.getMorePokemon(function(data){
+            console.log(data);
+            for(xPokemon in data){
+                //for each pokemon in result, add to allPokemon
+                allPokemon.push(new Pokemon(data[xPokemon]['name'], null, null));
+            }
             
-            api.getPokemonByName(self.selectedPokemon, function(pokemon){
-                self.selectedPokemon = pokemon;
-                //self.refreshDetailPage();
-            });
+            self.drawPokemonList();
         });
-
-        // $('.pId'+pokemon.name).bind('click', function(e) {
-		// 	//observer.setActiveGame(observer.activeGame._id);
-        //     alert("test")
-        //     alert("selected id: "+ e.target.id);
-        //     self.changeToDetailPage(e.target.id);
-		// });
-        
-        //$("#pokemon-list ul li:last").after('<li>Message</li>');
-        //$("#pokemon-list").append("<li>def</li>");
     }
     
-    //at end
-    // self.bindEvents();
-
-   	api.getAllPokemon(function(pokemon){
-           var xPokemon;
-		for(xPokemon in pokemon){
-            self.addPokemonToList(pokemon[xPokemon]);
+    self.drawDetailView = function(){
+        //change pokemon name
+        $('#pokemon-name').html(self.selectedPokemon.name);
+        
+        //clear lists
+        $('#types').empty();
+        $('#moves').empty();
+        
+        //add types
+        console.log(self.selectedPokemon.types);
+        if(self.selectedPokemon.types != null){
+            $('#types').append("<li data-role='list-divider' data-theme='b'>Types</li>");
+            for(xType in self.selectedPokemon.types){
+                console.log(self.selectedPokemon);
+                console.log(self.selectedPokemon.types);
+                console.log(self.selectedPokemon.types[xType]);
+                $('#types').append("<li>"+ self.selectedPokemon.types[xType] +"</li>")
+            }
         }
-        $('#pokemon-list').listview('refresh');
-	 });
-     
-    $(document).on( "pageinit", "#details-page", function( e ) {
-        var passedId = (($(this).data("url").indexOf("?") > 0) ? $(this).data("url") : window.location.href ).replace( /.*id=/, "" );
-        $("#details").html(["Selected id is: '", passedId, "'"].join(""));
-    });
+        
+        //add moves
+        if(self.selectedPokemon.types != null){
+            $('#moves').append("<li data-role='list-divider' data-theme='b'>Moves</li>");
+            for(xMove in self.selectedPokemon.moves){
+                $('#moves').append("<li>"+ self.selectedPokemon.moves[xMove] +"</li>")
+            }
+        }
+        
+        //refresh lists
+        $('#types').listview('refresh');
+        $('#moves').listview('refresh');
+    }
+    
+    self.updateUsername = function(){
+        $('#pokedex').html(localStorage.getItem("name") + "\'s pokedex");
+    }
+    
+    self.requestSinglePokemon = function(){
+        api.getPokemonByName(self.selectedPokemon.name, function(data){
+            for(xPokemon in allPokemon){
+                if(data['name'] == allPokemon[xPokemon].name){
+                    //found required pokemon
+                    allPokemon[xPokemon].types = data['types'];
+                    allPokemon[xPokemon].moves = data['moves'];
+                }
+            }
+            self.drawDetailView();
+        });
+    }
+    
+    self.getNewPokemon = function(pokemonName){
+        //var allPokemon = localStorage.getItem("pokemon");
+        for(xPokemon in allPokemon){
+            if(pokemonName == allPokemon[xPokemon].name){
+                return allPokemon[xPokemon];
+            }
+        }
+        //No pokemon found!
+        return null;
+    }
+    
+    self.addClickEventsToList = function(){
+        $('#pokemon-list').on('click', function(e){
+            e.preventDefault();
+            var id = e.target.parentElement.id;
+            if(id != "load"){
+                self.selectedPokemon = self.getNewPokemon(id);
+                $.mobile.changePage('#detail-page');
+            }
+        });
+        
+        $('#settings-button').on('click', function(e){
+            e.preventDefault();
+            self.onPokemonListPage = false;
+            $.mobile.changePage('#settings-page');
+            //alert("setting called");
+            //localStorage.setItem("name", "My");
+            //self.updateUsername();
+        });
+        
+        $('#save-button').on('click', function(e){
+            e.preventDefault();
+            var value = $("#username").val();
+            
+            if(value.length >= 5 && value.length <= 10){
+                localStorage.setItem("name", value);
+                self.updateUsername();
+                self.onPokemonListPage = true;
+                $.mobile.changePage('#index-page');
+            } else {
+                alert("you need to give a name between 5 and 10 characters");
+            }
+            
+            //$.mobile.changePage('#settings-page');
+            //alert("setting called");
+            //localStorage.setItem("name", "My");
+            //self.updateUsername();
+        });
+    }
+    
+    self.bindEvents();
     
 }
-    // ////////////////////////
-    
-	// self.refresh = function(){
-	// 	self.currentGames = api.getGames(function(games){
-	// 		self.currentGames = games;
-	// 		view.drawScreen();
-	// 	});
-	// }
-
-	// self.setActiveGame = function(gameId){
-	// 	api.getGame(gameId, function(myActiveGame){
-	// 		var changed = false;
-	// 		if(self.myActiveGame == null){
-	// 			changed = true;
-	// 		} else if(self.ActiveGame.id != myActiveGame) {
-	// 			changed = true;
-	// 		}
-			
-	// 		self.activeGame = myActiveGame;
-			
-	// 		if(changed){
-	// 			if(self.activeGame.status == "setup"){
-	// 				var cols="abcdefghij";
-	// 				ships = [];
-	// 				cells = [];
-	// 				for(var y = 1; y <= 10; y++){
-	// 					for (x in cols){
-	// 						cells.push(new Cell(cols.charAt(x), y));
-	// 					}
-	// 				}
-	// 				self.activeGame.myGameboard = new GameBoard(null, ships, [], cells);
-	// 			}
-	// 		}
-	// 		self.refresh();
-	// 	})
-	// }
-
-	// self.shoot = function(x, y){
-	// 	api.shoot(self.activeGame, new Cell(x,y), function(returnData){;
-	// 		self.setActiveGame(self.activeGame._id);
-	// 	});
-	// }
-	
-	// self.addShip = function(ship){
-	// 	self.activeGame.myGameboard.ships.push(ship);
-	// }
-	
-	// self.pushShip = function(ship){
-	// 	var found = false;
-	// 	$.grep(self.activeGame.myGameboard.ships, function(e){
-	// 		if(e._id == ship._id){
-	// 			e = ship;
-	// 			found = true;
-	// 		}
-	// 	});
-	// 	if(!found){
-	// 		self.activeGame.myGameboard.ships.push(ship);
-	// 	}
-	// }
-	
-	// self.submitBoard = function(){
-	// 	var everythingOk = false;
-	// 	if(self.activeGame.myGameboard.ships.length == 5){
-	// 		api.submitBoard(self.activeGame, function(data){
-	// 			api.getGame(self.activeGame._id, function(gameData){
-	// 				everythingOk = true;
-	// 				self.activeGame = gameData;
-	// 				self.refresh();
-	// 			});
-	// 		});
-	// 	} else {
-	// 		alert("please place all ships on the field", self.activeGame.myGameboard.ships.length);
-	// 	}
-	// }
-	// self.newGame = function(){
-	// 	api.newGame(function(data){
-	// 		self.refresh();
-	// 	});
-	// }	
-
-	// self.newComputerGame = function(){
-	// 	api.newComputerGame(function(data){
-	// 		self.refresh();
-	// 	});
-	// }	
-
-	// self.deleteAllGames = function(){
-	// 	api.clearGamelist(function(){
-	// 		self.refresh();
-	// 	});
-	// }
-
-	// var view = new View(self);
-	
-	// api.getShips(function(boats){
-	// 	self.boats = boats;
-	// });
-	
-	// self.refresh();
-
-
-
-*/
-
-
